23e313c2d764ebbb4cfe74a68f7972efd1fb6b43
Shreyans Tiwari <shreyans.tiwari33@gmail.com>
2023-01-19 19:44:17 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members:[
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us8.api.mailchimp.com/3.0/lists/1332cb3e86";
+
+    const options = {
+        method: "POST",
+        auth: "shrey:fd4062dd0510b4a284d52e071509566a-us8"
+    };
+
+    const request = https.request(url, options, function(response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
+
+app.listen(3000, function() {
+    console.log("Server running on port 3000");
+});
+
+
+// API KEY
+// fd4062dd0510b4a284d52e071509566a-us8
+
+// Audience ID
+// 1332cb3e86
